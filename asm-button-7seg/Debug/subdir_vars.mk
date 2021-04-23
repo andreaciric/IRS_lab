14a767782c743a41bfbcf9acd901176819183fd6
Andrea Ciric <andreaciric23@gmail.com>
2021-04-23 13:28:58 +0200

    Add files via upload

---
+++ asm-button-7seg/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+ASM_SRCS += \
+../function.asm \
+../main.asm 
+
+OBJS += \
+./function.obj \
+./main.obj 
+
+ASM_DEPS += \
+./function.d \
+./main.d 
+
+OBJS__QUOTED += \
+"function.obj" \
+"main.obj" 
+
+ASM_DEPS__QUOTED += \
+"function.d" \
+"main.d" 
+
+ASM_SRCS__QUOTED += \
+"../function.asm" \
+"../main.asm" 
+
+
---
+++ asm-button-7seg/function.asm
@@ -0,0 +1,68 @@
+; Function that returns 7 segment display from certain digit
+
+			.cdecls C,LIST,"msp430.h"       ; Include device header file
+
+			.def	WriteLed
+
+			.sect	.const
+
+segtab2		.byte	0x48					; digit 0-9 table
+			.byte	0x40
+			.byte	0x08
+			.byte	0x40
+			.byte	0x40
+			.byte	0x40
+			.byte	0x48
+			.byte	0x40
+			.byte	0x48
+			.byte	0x40
+
+segtab3		.byte	0x80					; digit 0-9 table
+			.byte	0x00
+			.byte	0x80
+			.byte	0x80
+			.byte	0x80
+			.byte	0x80
+			.byte	0x80
+			.byte	0x80
+			.byte	0x80
+			.byte	0x80
+
+segtab4		.byte	0x09					; digit 0-9 table
+			.byte	0x08
+			.byte	0x08
+			.byte	0x08
+			.byte	0x09
+			.byte	0x01
+			.byte	0x01
+			.byte	0x08
+			.byte	0x09
+			.byte	0x09
+
+segtab8		.byte	0x02					; digit 0-9 table
+			.byte	0x00
+			.byte	0x06
+			.byte	0x06
+			.byte	0x04
+			.byte	0x06
+			.byte	0x06
+			.byte	0x00
+			.byte	0x06
+			.byte	0x06
+
+			.text
+WriteLed	bis.b 	#0x48, &P2OUT
+			bic.b	segtab2(R12), &P2OUT
+
+			bis.b 	#0x80, &P3OUT
+			bic.b	segtab3(R12), &P3OUT
+
+			bis.b 	#0x09, &P4OUT
+			bic.b	segtab4(R12), &P4OUT
+
+			bis.b 	#0x06, &P8OUT
+			bic.b	segtab8(R12), &P8OUT
+
+			ret
+
+			.end
